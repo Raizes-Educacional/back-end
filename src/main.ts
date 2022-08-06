6cd6a6434f868f9506a739eab2aa35801404b76c
Gabrielttbr <ttbr2888@gmail.com>
2022-08-06 20:35:19 -0300

    Habilitando configurações cors no nest

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,7 @@ import * as dotenv from 'dotenv';
 dotenv.config();
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors();
   await app.listen(process.env.PORT);
 }
 bootstrap();
